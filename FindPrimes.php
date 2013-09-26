a4d3c6d09721d413bd8caac83c4fdde495ac0212
Mirko Oleszuk <mirkoole@gmail.com>
2013-09-26 11:28:30 +0200

    code cleanups

--- FindPrimes.php
+++ FindPrimes.php
@@ -5,28 +5,32 @@ if(!function_exists("gmp_init")){
   exit("ERROR: Please install GMP!");
 }
 
-function findPrimes($numprimes, $min, $max){
-
-	$rand = mt_rand($min, $max);
-	$p = gmp_nextprime($rand);
-
-	for($i = 0; $i < $numprimes; $i++){
+/**
+* findPrimes() finds n primes in range [min,max] 
+* @param $n number of primes to find
+* @param $min lower limit of range
+* @param $max upper limit of range
+*/
+function findPrimes($n, $min, $max){
+	for($i = 0; $i < $n; $i++){
 		$p = mt_rand($min, $max);
 		$p = gmp_nextprime($p);
 		echo gmp_strval($p)."\n";
 	}
 }
 
-// save start time
-$start = microtime(true);
-
 // set parameter
-$numprimes = 1000;
+$n = 1000;
 $min = 1000000000;
 $max = 9999999999;
 
-findPrimes($numprimes, $min, $max);
+// save start time
+$start = microtime(true);
 
-echo "Primes (".$numprimes.") found in: " . round((microtime(true) - $start), 3) . " s\n";
+// run it
+findPrimes($n, $min, $max);
+
+// runtime
+echo "Primes (".$n.") found in: " . round((microtime(true) - $start), 3) . " s\n";
 
 ?>
